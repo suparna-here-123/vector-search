66eb4f2a34bad673ed351879ca04959b37b4c286
suparna-here-123 <suparnaprasad.in@gmail.com>
2025-10-13 10:43:42 +0530

    One iteration of K-means clustering + batch upsert works. Now implementing search

--- indexes.py
+++ indexes.py
@@ -4,12 +4,13 @@ import numpy as np
 from shortuuid import uuid
 from random import randint
 from utils import *
+from abc import ABC, abstractmethod
 
-class VanillaIndex :
-    def __init__(self, index_name:str, index_dim=512, index_type='IVFFlat', index_path=f"/home/suppra/Desktop/pinecone/Indexes") :
+class VanillaIndex(ABC) :
+    def __init__(self, index_name:str, index_dim=512, index_type=str, index_path=f"/home/suppra/Desktop/pinecone/Indexes") :
         self.index_name = index_name
         self.index_dim = index_dim
-        self.index_path = f'index_path/{index_type}_{index_name}'
+        self.index_path = f'{index_path}/{index_type}_{index_name}'
         self.vectorType = [('_id', 'i4'), ('vector', int, (4,)), ('metadata', dict)]
             
         # Create new index if it doesn't exist
@@ -19,16 +20,18 @@ class VanillaIndex :
             # Creating a descriptor for the index
             self.descriptor = {"Name" : self.index_name, 
                                "Dimensions" : self.index_dim, 
-                               "Namespaces" : {}}       
+                               "Namespaces" : {}}
+            self.flushIndex()    
         else :
-            self.descriptor = json.loads(open(f'{self.index_path}/descriptor.json', 'a').read())
+            self.descriptor = json.loads(open(f'{self.index_path}/descriptor.json', 'r').read())
     
     def getNamespace(self, namespace='default') :
-        '''Check if a namespace exists, else create one and return its path'''
+        '''Return path of a namespace, if it's non-existent, create one.'''
         namespacePath = f'{self.index_path}/{namespace}'
         if not os.path.isdir(namespacePath) :
             os.mkdir(namespacePath)
-            self.descriptor['Namespaces'][namespace] = {'Vectors' : 0, 'Centroid' : ''}
+            self.descriptor['Namespaces'][namespace] = {'Vectors' : 0, 'Clusters' : {}}
+            self.flushIndex()
         return namespacePath
 
     def upsert(self, vector, namespace='default') :
@@ -36,9 +39,23 @@ class VanillaIndex :
         namespacePath = self.getNamespace(namespace=namespace)
         try :
             vecID = uuid()
-            np.save(f'{namespacePath}/uuid.npy', vector)
+
+            # Save vector into namespace
+            np.save(f'{namespacePath}/{vecID}.npy', vector)
+
+            # Increase count of vectors in namespace by 1
             self.descriptor['Namespaces'][namespace]['Vectors'] += 1
+            self.flushIndex()
+
+
+            # Should assign it to the right cluster
+
+
+
+
+
             return 1
+        
         except Exception as e :
             print(str(e))
             return 0
@@ -48,25 +65,138 @@ class VanillaIndex :
         for vec in vectors :
             res = self.upsert(vector=vec, namespace=namespace)
             if not res :
+                print('single upsert failed')
                 return res
-            
-    def flushIndex(self) :
-        '''Run when you're done operating with the index.'''
-        with open(f'{self.index_path}/descriptor.json', 'a') as desc :
-            desc.write(json.dumps(self.descriptor))
     
+
+        # Check if threshold for clustering has been reached
+        self.organise(namespace)
+        
+        # Call cluster operation (implementation depends on type of index)
+        return 1
+
+    @abstractmethod
+    def organise(self, namespace:str, min_threshold:int) :
+        '''Organisation of vectors in a namespace - dependent on type of index'''
+        pass
+    
+    @abstractmethod
+    def search(self, namespace:str, query, topK:int) :
+        '''Given a query vector, return the top K most similar vectors along w cosine similarity'''
+        pass
+
+    def flushIndex(self) :
+        '''Run when description needs to be written to disk'''
+        with open(f'{self.index_path}/descriptor.json', 'w') as desc :
+            desc.write(json.dumps(self.descriptor, indent=4))
+    
+    def saveClusters(self, namespace:str, namespacePath:str, clusters:list[dict]) :
+        try :
+            for clusterInd, cluster in enumerate(clusters) :
+                # Create new directory for cluster if it doesn't exist
+                clusterPath = f'{namespacePath}/cluster_{clusterInd}'
+                if not os.path.isdir(clusterPath) :
+                    os.mkdir(clusterPath)
+                    print(f"Made path {clusterPath}")
+                
+                # Save center
+                np.save(f'{clusterPath}/center.npy', cluster['center'])
+                self.descriptor['Namespaces'][namespace]['Clusters'].update({f'C{clusterInd}' : clusterPath})
+                self.flushIndex()
+
+                # Save vectors
+                for vec in cluster['vectors'] :
+                    np.save(f'{clusterPath}/{uuid()}.npy', vec)
+            
+            # Delete the now duplicated vectors under the namespace
+            for item in os.listdir(namespacePath) :
+                item_path = os.path.join(namespacePath, item)
+                if os.path.isfile(item_path) :
+                    os.remove(item_path)
+            
+            # Return success code
+            return 1
+    
+        except Exception as e :
+            print(f'The error is : {e}')
+            return 0          
+            
 
 class IVFFlat(VanillaIndex) :
     def __init__(self, index_name:str) :
         self.index_type = 'IVFFlat'
-        super.__init__(index_name=index_name, index_type = self.index_type)
+        super().__init__(index_name=index_name, index_type = self.index_type)
 
-    def cluster(self, namespace='default', min_threshold=30) :
-        '''Create clusters only when there are mininum number of vectors in a namespace
-            Called after upsert operation'''
-        namespacePath = f'{self.index_path}/{namespace}'
+    def organise(self, namespace='default', min_threshold=8) :
+        '''
+        Create clusters only when there are mininum number of vectors in a namespace.
+        Called after upsert operation.
+        '''
         lenVectors = self.descriptor['Namespaces'][namespace]['Vectors']
         if lenVectors >= min_threshold :
-            self.KMeansClustering(namespacePath, k=4)
+            # If first-time clustering...
+            if not self.descriptor['Namespaces'][namespace].get('Clusters', None) :
+                namespacePath = f'{self.index_path}/{namespace}'
+                clusters = KMeansClustering(namespacePath, k=4)
+                if self.saveClusters(namespace, namespacePath, clusters) :
+                    return 1
+
+            # Clusters already exist :
+            else :
+                print("Do something")
+        
         else : 
-            return
+            return 0
+    
+    def search(self, query, namespace='default', topK=3) :
+        # Compare vector with all centroids in this namespace and find nearest one
+        Q = query[0][1]
+        allNamespaces = self.descriptor['Namespaces']
+        for ns in allNamespaces :
+            clusters = ns.get('Clusters', None)
+
+            # By default, search the whole namespace if there are no clusters yet
+            searchPath = self.getNamespace(namespace)
+
+            # If there are clusters, search in closest one
+            if clusters :
+                nearestClusterPath, distance = nearestCluster(query, clusters)
+                print(f"Nearest cluster = {nearestClusterPath} at distance {distance}")
+                searchPath = nearestClusterPath
+                
+            # Do brute-force search/sort
+            vec_dist = []
+            for vecPath in os.listdir(searchPath) :
+                vec = np.load(vecPath, allow_pickle=True)
+                vec_dist.append(vec, euclideanDistance(Q, vec[0][1]))
+            
+            # Sort vectors by distance from query
+            vec_dist.sort(key=lambda item : item[1])
+
+            # Return top K vectors with their distances from query
+            return vec_dist[:topK]
+  
+if __name__ == "__main__" :
+    custom = [('_id', 'i4'), ('vector', int, (4,)), ('metadata', dict)]
+
+    a1 = np.array([1,2,3,4])
+    a2 = np.array([1,2,5,4])
+    a3 = np.array([1,12,3,4])
+    a4 = np.array([1,12,3,6])
+    a5 = np.array([1,2,8,4])
+    a6 = np.array([1,2,8,5])
+    a7 = np.array([0,12,2,4])
+    a8 = np.array([0,11,3,6])
+
+    v1 = np.array([(1, a1, {'docId' : 15, 'type' : 'political'})], dtype=custom)
+    v2 = np.array([(2, a2, {'docId' : 13, 'type' : 'history'})], dtype=custom)
+    v3 = np.array([(3, a3, {'docId' : 15, 'type' : 'economics'})], dtype=custom)
+    v4 = np.array([(4, a4, {'docId' : 15, 'type' : 'biology'})], dtype=custom)
+    v5 = np.array([(1, a5, {'docId' : 14, 'type' : 'political'})], dtype=custom)
+    v6 = np.array([(2, a6, {'docId' : 12, 'type' : 'history'})], dtype=custom)
+    v7 = np.array([(3, a7, {'docId' : 12, 'type' : 'physics'})], dtype=custom)
+    v8 = np.array([(4, a8, {'docId' : 15, 'type' : 'biology'})], dtype=custom)
+
+    idx = IVFFlat('ABC')
+    resUpsert = idx.upsert_batch([v1, v2, v3, v4, v5, v6, v7, v8], namespace='users')
+    print('Upsert operation success : ', bool(resUpsert))
--- utils.py
+++ utils.py
@@ -1,36 +1,74 @@
 import os
 from math import sqrt
-from random import randint
+from random import sample
 import numpy as np
 
-def KMeansClustering(self, path:str, k:int) :
+def KMeansClustering(path: str, k: int):
     '''Creates k clusters for a given directory(namespace/cluster)'''
     # 1) Bring vectors in path into memory
     vectors = []
-    for file in os.path.listdir(path) :
-        vectors.append(np.load(file, allow_pickle=True))
+    for file in os.listdir(path) :
+        vectors.append(np.load(os.path.join(path, file), allow_pickle=True))
+
+    # 2) Choose k random vectors to be cluster centers - IMPROVISE
+    centers = sample(vectors, 3)
     
-    # 2) Choose k random vectors to be cluster centroids - IMPROVISE
-    centroidIndexes = randint(0, len(vectors)-1)
-    centroids = []
-    for centroidIndex in centroidIndexes :
-        centroids.append(vectors.pop(centroidIndex))
-    
-    # 3) Assign vectors to centroids
+    # 3) Assign vectors to centers
+    clusters = getClusters(vectors, centers)
 
-def assignToCluster(self, vectors, centroids) :
-        numCentroids = len(centroids)
-        for vec in vectors :
-            distFromCentroids = [0] * numCentroids
+    # 4) Update centers for N times
+    updateCenters(clusters)
 
-            # Calculate distance of this vector from all centroids
-            for centroidIndex in range(numCentroids):
-                dis = euclideanDistance(centroids[centroidIndex][0], vec)
-                distFromCentroids[centroidIndex] = dis
+    # 5) Return clusters
+    return clusters
+
+def getClusters(vectors, centers) :
+        print(f"Inside getClusters - received {len(vectors)} vectors")
+        # Create a cluster with center:vector key-value pair
+        numCenters = len(centers)
+        
+        # Initialize the cluster
+        clusters = [{'center' : centers[i], 'vectors' : []} for i in range(numCenters)]
+        
+        # Assign vectors to closest center
+        for i, vector in enumerate(vectors) :
+            justVector = vector[0][1]                 
+            distFromcenters = [0] * numCenters
+
+            # Calculate distance of this vector from all centers
+            for centerIndex in range(numCenters):
+                justCenter = centers[centerIndex][0][1]
+                dis = euclideanDistance(justCenter, justVector)
+                distFromcenters[centerIndex] = dis
             
-            curr_cluster = np.argmin(distFromCentroids)
-            
+            toCluster = np.argmin(distFromcenters)
+            clusters[toCluster]['vectors'].append(vector)
 
-def euclideanDistance(vec1, vec2) :
-    v1, v2 = vec1[1], vec2[1]
+        return clusters
+
+def updateCenters(clusters) :
+    for cluster in clusters :
+        justVectors = np.array([vec[0][1] for vec in cluster['vectors']])
+        new_center = justVectors.mean(axis=0)
+
+        # Cluster center will not have any metadata - just an ndarray
+        cluster['center'] = new_center
+        print(new_center)
+
+def euclideanDistance(v1, v2) :
     return np.sqrt(np.sum((v1 - v2) ** 2))
+
+def nearestCluster(v, clusters:dict) :
+    '''Return the path and distance of the nearest cluster whose centroid is closest to given query vector'''
+    cPaths = []
+    distances = []
+    # Iterating over every cluster's centroid and finding query vector distance from it
+    for c, cPath in clusters.items() :
+        cPaths.append(cPath)
+        centroid = np.load(f'{cPath}/center.py', allow_pickle=True)
+        dist = euclideanDistance(v[0][1], centroid[0][1])
+        distances.append(dist)
+    
+    # Return path of the cluster whose centroid is closest to query vector
+    closestClusterIndex = np.argmin(distances)
+    return [cPaths[closestClusterIndex], distances[closestClusterIndex]]
